7b6c9879e03d505c51a287fcc3d31e3875ffee83
ekaterina-chernykh <ekaterina-chernykh@users.noreply.github.com>
2022-10-14 03:50:17 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // This file contains the JS functions for index.html
 
 /**
-* This function gets users street number and name back to user.
-    */
+ * This function gets users street number and name back to user.
+ */
 function enterClicked() {
-    // input
-    const streetnumber = parseInt(document.getElementById("street-number").value)
-    const streetname = document.getElementById("street-name").value
+  // input
+  const streetnumber = parseInt(document.getElementById("street-number").value)
+  const streetname = document.getElementById("street-name").value
 
-    // output
-    document.getElementById("address").innerHTML = 
-        "Your address is: " + streetnumber + ", " + streetname + "."
+  // output
+  document.getElementById("address").innerHTML =
+    "Your address is: " + streetnumber + ", " + streetname + "."
 }
